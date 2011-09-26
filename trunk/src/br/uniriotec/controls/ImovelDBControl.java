c72fff3e1ab49d551e96ea75cd7d28affcdc0b70
jccprj <jccprj@6181dff1-6df9-0bf2-5f29-899187a42980>
2011-09-26 00:55:39 +0000

    Finalizacao do trabalho 1

--- trunk/src/br/uniriotec/controls/ImovelDBControl.java
+++ trunk/src/br/uniriotec/controls/ImovelDBControl.java
@@ -11,7 +11,7 @@ import br.uniriotec.beans.Imovel;
 @JdbcControl.ConnectionDataSource(jndiName = "imovelDS")
 public interface ImovelDBControl extends JdbcControl {
 
-	@JdbcControl.SQL(statement="SELECT id, endereco, preco, quartos, sol, possui_vaga AS possuivaga FROM tb_imovel "
+	@JdbcControl.SQL(statement="SELECT id, endereco || ' ' || BAIRRO || ' ' || CIDADE as endereco, preco, quartos, sol, possui_vaga as POSSUIVAGA FROM tb_imovel "
 		+ " WHERE 1=1 {sql: p_where} {sql: p_orderby} " )
 	Imovel[] getImoveis(String p_where, String p_orderby) throws SQLException;
 	 
